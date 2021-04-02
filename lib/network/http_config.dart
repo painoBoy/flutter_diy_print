5f37616d5a1470ab5877b1270ccdc21b82a96781
myProject <735294631@qq.com>
2021-04-02 17:59:17 +0800

    Update http_config.dart

--- lib/network/http_config.dart
+++ lib/network/http_config.dart
@@ -1,6 +1,5 @@
 class Config {
-  static String BASE_URL = "https://release.xiaohaima.top:8772/";
+  static String BASE_URL = "接口地址";
   static int TIMEOUT = 5000;
-  // static String BASE_URL = "http://jd.itying.com/api/plist";
 
 }
